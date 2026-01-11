ad356c4b8e44281f91131ff0302d34bd8e96f89e
BEILIYA <zacyzq@gmail.com>
2026-01-11 14:21:32 +0000

    move report code files

---
+++ 03. Create/03.b Report/3.b.2 Testing/Model_improved.py
@@ -0,0 +1,99 @@
+'''
+The improved version of model, considering months.
+'''
+
+
+# first row in the dataset
+month = "jul"
+temp = 8.2
+wind = 6.7
+rh = 51 # relative humidity
+dmc = 26.2 # duff
+dc = 94.3 # drought
+ffmc = 86.2 # flammable litter
+
+
+# list of months
+months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+
+
+# helper to check negative values
+def anyneg(*args):
+    for i in args:
+        if i < 0:
+            return True
+    return False
+
+# main scoring function
+def scoring(month, temp, wind, rh, dmc, dc, ffmc):
+    # terminate scoring if:
+    # - numerical values (except temp) are negative
+    # - month not valid
+    if anyneg(wind, rh, dmc, dc, ffmc)\
+    or (month not in months):
+        print(-1)
+        print("invalid parameter")
+        return -1
+    # risk score
+    score = 100 # this will be changed later according to month factor
+
+    ''' 
+    most rules are inherited from AR1, while adding these:
+    - if jun-sep, score = 80 (big impact)
+    - if mar-may or oct or dec, score = 90 (medium impact)
+    - otherwise score = 100 (no changes)
+    '''
+    if month in months[5:9]:
+        score = 80
+    elif month in months[2:5]:
+        score = 90
+    
+
+    # temperature
+    if temp > 30:
+        score -= 30
+    elif temp > 25:
+        score -= 22
+    elif temp > 15:
+        score -= 10
+
+    # humidity
+    # low relevance thus lower scoring factors
+    if rh <= 30:
+        score -= 9
+    elif rh <= 50:
+        score -= 3
+
+    # wind (small impact)
+    if wind >= 6:
+        score -= 8
+    elif wind >= 4:
+        score -= 4
+
+    # drought (small impact)
+    score -= dc * (1/125)
+
+    # duff (small impact)
+    score -= dmc * (1/50)
+
+    # litter flammable (large impact)
+    if ffmc >= 90:
+        score -= 20
+    elif ffmc >= 80:
+        score -= 16
+
+    score = round(score, 3)
+    print(score)
+    # evaluating
+    # changed in AR3 due to distribution
+    if score <= 25:
+        print("high risk, action needed")
+    elif score <= 45:
+        print("medium risk")
+    else:
+        print("low risk")
+
+    return score
+
+if __name__ == "__main__":
+    scoring("jul", -temp, wind, rh, dmc, dc, ffmc) 
---
+++ 03. Create/03.b Report/3.b.3 Problems/microbit_serial.py
@@ -0,0 +1,47 @@
+# Basic requirement 2
+
+import csv
+import serial
+
+ser = serial.Serial()
+ser.baudrate = 115200
+# run "mode" in cmd to get this
+ser.port = "com3"
+# run ls /dev/cu.*  to get path below
+ser.port = "/dev/cu.usbmodem14102" # MacBook Only 
+ser.open()
+
+header = ['temp', 'rh', 'lighting', 'score']
+# Open the CSV file for writing
+with open('output.csv', 'w', newline='') as csvfile:
+    db = csv.writer(csvfile)
+    db.writerow(header)
+
+    while True:
+        microbitData = str(ser.readline())
+        print(microbitData)
+        
+        cleaned = microbitData
+        cleaned = microbitData[3:]
+        cleaned = cleaned.replace("'",'')
+        cleaned = cleaned.replace('\\r\\n','')
+        cleaned = cleaned.replace('\\n', '')
+        print(cleaned)
+        
+
+        cleaned = cleaned.split(" ")
+    
+        temp = cleaned[0]
+        rh = cleaned[1]
+        lighting = cleaned[2]
+        score = cleaned[3]
+
+        # Write data to the CSV file
+        db.writerow([temp, rh, lighting, score]) # Square Brackets to create list with single element
+        # Data written to .csv must be in a sequence {An iterable object that can be looped through}.
+        csvfile.flush()  # Flush the data to ensure that it is written to the file
+        
+time.sleep(5)
+ser.close()
+
+
