4be4cf6ae734b112526d31c74615fafcbb452ce0
BEILIYA <zacyzq@gmail.com>
2026-01-26 11:17:56 +0000

    remove outlier

--- 03. Create/03.a Practical/Modelling/Cleaning.py
+++ 03. Create/03.a Practical/Modelling/Cleaning.py
@@ -17,14 +17,24 @@ df.drop("Y", axis=1, inplace=True)
 df.drop("rain", axis=1, inplace=True)
 df.drop("day", axis=1, inplace=True)
 
+
+
 # add risk column
 def risk_def(isi, area):
     if area > 0:
         return isi * 1.2
     return isi
 
+outlier_index = 0
 for i in df.index:
-    df.loc[i, "risk"] = round(risk_def(df.loc[i, "ISI"], df.loc[i, "area"]), 1)
+    risk = round(risk_def(df.loc[i, "ISI"], df.loc[i, "area"]), 1)
+    df.loc[i, "risk"] = risk
+    # clean the one outlier
+    # this was identified in graphs
+    if risk > 50:
+        outlier_index = i
+df = df.drop(outlier_index)
+
 
 # output cleaned file
 df.to_csv("forestfires_cleaned.csv", index=False)
