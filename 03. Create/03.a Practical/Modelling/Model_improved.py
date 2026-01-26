49bc8e6949ca09d166107616048df86b671ef678
BEILIYA <zacyzq@gmail.com>
2026-01-26 11:54:42 +0000

    clean AR3

--- 03. Create/03.a Practical/Modelling/Model_improved.py
+++ 03. Create/03.a Practical/Modelling/Model_improved.py
@@ -1,22 +1,7 @@
-'''
-The improved version of model, considering months.
-'''
+# AR3
 
 from Model import scoring
 import csv
-
-# first row in the dataset
-month = "" # to be inputted in demonstration
-temp = 8.2
-wind = 6.7
-rh = 51 # relative humidity
-dmc = 26.2 # duff
-dc = 94.3 # drought
-ffmc = 86.2 # flammable litter
-
-
-# list of months
-months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
 
 print("The programs reads the sample readings and simulates adaptive analysis.")
 
@@ -31,19 +16,11 @@ with open("sample_readings.csv", 'r') as file:
     # discard the first line
     next(csvFile)
     for line in csvFile:
-        for i in range(1, len(line)):
+        for i in range(1, len(line)): # the first string is month, ignore
             line[i] = float(line[i])
         score = scoring(*line)
         print(f"This is input number {lines_count+1}.")
-        print(f"Compare the input number {lines_count}, the risk has deviated by {round(score-last_score, 2)}.")
+        print(f"Compared to input number {lines_count}, the risk has deviated by {round(score-last_score, 2)}.")
         last_score = score
         lines_count += 1
 
-'''
-if __name__ == "__main__":
-    date = datetime.datetime.now() # get current date
-    month = months[date.month-1] # get corresponding month value
-    scoring(month, temp, wind, rh, dmc, dc, ffmc) 
-    '''
-
-# read a file
