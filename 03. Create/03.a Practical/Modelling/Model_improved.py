ce033fd354656ac8fe5c3d1b120f2e0c83b26547
BEILIYA <zacyzq@gmail.com>
2026-01-26 11:52:07 +0000

    rewrite AR3

--- 03. Create/03.a Practical/Modelling/Model_improved.py
+++ 03. Create/03.a Practical/Modelling/Model_improved.py
@@ -2,7 +2,8 @@
 The improved version of model, considering months.
 '''
 
-import datetime
+from Model import scoring
+import csv
 
 # first row in the dataset
 month = "" # to be inputted in demonstration
@@ -17,86 +18,32 @@ ffmc = 86.2 # flammable litter
 # list of months
 months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
 
+print("The programs reads the sample readings and simulates adaptive analysis.")
 
-# helper to check negative values
-def anyneg(*args):
-    for i in args:
-        if i < 0:
-            return True
-    return False
+# lines read
+lines_count = 0
 
-# main scoring function
-def scoring(month, temp, wind, rh, dmc, dc, ffmc):
-    # terminate scoring if:
-    # - numerical values (except temp) are negative
-    # - month not valid
-    if anyneg(wind, rh, dmc, dc, ffmc)\
-    or (month not in months):
-        print(-1)
-        print("invalid parameter")
-        return -1
-    # risk score
-    score = 100 # this will be changed later according to month factor
+# last score calculated
+last_score = 0
 
-    ''' 
-    most rules are inherited from AR1, while adding these:
-    - if jun-sep, score = 80 (big impact)
-    - if mar-may or oct or dec, score = 90 (medium impact)
-    - otherwise score = 100 (no changes)
-    '''
-    if month in months[5:9]:
-        score = 80
-    elif month in months[2:5]:
-        score = 90
-    
+with open("sample_readings.csv", 'r') as file:
+    csvFile = csv.reader(file)
+    # discard the first line
+    next(csvFile)
+    for line in csvFile:
+        for i in range(1, len(line)):
+            line[i] = float(line[i])
+        score = scoring(*line)
+        print(f"This is input number {lines_count+1}.")
+        print(f"Compare the input number {lines_count}, the risk has deviated by {round(score-last_score, 2)}.")
+        last_score = score
+        lines_count += 1
 
-    # temperature
-    if temp > 30:
-        score -= 30
-    elif temp > 25:
-        score -= 22
-    elif temp > 15:
-        score -= 10
-
-    # humidity
-    # low relevance thus lower scoring factors
-    if rh <= 30:
-        score -= 9
-    elif rh <= 50:
-        score -= 3
-
-    # wind (small impact)
-    if wind >= 6:
-        score -= 8
-    elif wind >= 4:
-        score -= 4
-
-    # drought (small impact)
-    score -= dc * (1/125)
-
-    # duff (small impact)
-    score -= dmc * (1/50)
-
-    # litter flammable (large impact)
-    if ffmc >= 90:
-        score -= 20
-    elif ffmc >= 80:
-        score -= 16
-
-    score = round(score, 3)
-    print(score)
-    # evaluating
-    # changed in AR3 due to distribution
-    if score <= 25:
-        print("high risk, action needed")
-    elif score <= 45:
-        print("medium risk")
-    else:
-        print("low risk")
-
-    return score
-
+'''
 if __name__ == "__main__":
     date = datetime.datetime.now() # get current date
     month = months[date.month-1] # get corresponding month value
     scoring(month, temp, wind, rh, dmc, dc, ffmc) 
+    '''
+
+# read a file
